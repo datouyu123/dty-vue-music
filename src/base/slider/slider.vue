e888cc958ba3cd1ad915dce49f6c6fc25b378fe3
datouyu123 <391426555@qq.com>
2018-04-13 18:53:54 +0800

    后端接口代理取数据(axios)

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -119,6 +119,9 @@ export default {
         this.slider.goToPage(pageIndex, 0, 400)
       }, this.interval)
     }
+  },
+  destroyed() {
+    clearTimeout(this.timer)
   }
 }
 
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -20,7 +20,7 @@
 
 <script type="text/ecmascript-6">
 import Slider from 'base/slider/slider'
-import {getRecommend} from 'api/recommend'
+import {getRecommend, getDiscList} from 'api/recommend'
 import {ERR_OK} from 'api/config'
 
 export default {
@@ -33,6 +33,7 @@ export default {
   },
   created() {
     this._getRecommend()
+    this._getDiscList()
   },
   mounted() {},
   methods: {
@@ -40,6 +41,14 @@ export default {
       getRecommend().then((res) => {
         if (res.code === ERR_OK) {
           this.recommends = res.data.slider
+        }
+      })
+    },
+    _getDiscList() {
+      getDiscList().then((res) => {
+        console.log('res: ' + res)
+        if (res.code === ERR_OK) {
+          console.log(res.data.list)
         }
       })
     }
